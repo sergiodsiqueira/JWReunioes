3fd485ffb80b3c63b3e0c68acb47f9f3f45fc4ea
sergio.siqueira <sergio.siqueira@totvs.com.br>
2025-03-12 14:33:02 -0300

    Ajuste DOTENV

--- config/jwt.js
+++ config/jwt.js
@@ -1,4 +1,8 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 module.exports = {
-    secret: process.env.JWT_SECRET || 'jehovahs_witnesses_1914',
+    secret: process.env.JWT_SECRET,
     expiresIn: '1h' // Token válido por 1 hora
   };
